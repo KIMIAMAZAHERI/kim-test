6d5d870d82053e1568088ac42dcc7d0baad65873
kimia mazaheri <mazaherikimia@gmail.com>
2022-06-03 00:39:07 +0200

    Login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Poke from "./images/poke.png";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      Test test test
+    <div className="main">
+     <div className='sub-main'>
+       <div>
+          <div className='imgs'>
+            <div className='container-image'>
+            <img src={Poke} alt="picture of charmander" className='poke'></img>
+            </div>
+       </div>
+       <div>
+         <h1>Login page</h1>
+         <Login />
+       </div>
+       </div>
+     </div>
     </div>
   );
 }
